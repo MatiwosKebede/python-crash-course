cf91f8155f6e90c046d0bcdd5cc984e200407e02
Matiwos Kebede <matiwoskebede01@gmail.com>
2025-07-07 07:46:24 +0300

    exercise 3.8 is completed

--- chapter 3/03_full_of_chapter_3_exercise.py
+++ chapter 3/03_full_of_chapter_3_exercise.py
@@ -11,7 +11,7 @@ print(names[2]) #Output James
 print(names[3]) #Output Abel
 
 # exericse 3.2 Greating 
-print("Exercise 3.2")
+print("\n\n\nExercise 3.2")
 '''Start with the list you used in Exercise 3.1, but instead of just printing each person's name, print a message to them.
 The text of each message should be the samae , but each message should be personalized with the person's name'''
 print(f"Hi {names[0]}, How was your day!")
@@ -20,7 +20,7 @@ print(f"Hi {names[2]}, How was your day?")
 print(f"Hi {names[3]}, How was the your day")
 
 # exercise 3.3 Your Own List
-print("Exercise 3.3")
+print("\n\n\nExercise 3.3")
 
 '''Think of your favorite mdoe of transportation, such as a motorcycle or a car, and make a list that stores several examples/
 Use your list to print a series of statements about these items, such as "I would like to own a Honda motorcycle.'''
@@ -29,7 +29,7 @@ message = ["I would like to own a ", "I would like to own an "]
 print(message[0] + cars[0])
 
 # exercise 3.4 Guest list
-print("Exercise 3.4")
+print("\n\n\nExercise 3.4")
 
 '''If you could invite anyone, living or deceased, to dinner, who would you invite?
 Make a list that includes at least three people you'd like to invite to dinner. Then use your list to print a message to each person, inviting them to dinner.'''
@@ -43,7 +43,7 @@ print(f"{date_str.rjust(50)} \nHello Mr {invite_names[1]} {invitation_message}")
 print(f"{date_str.rjust(50)} \nHello Mr {invite_names[2]} {invitation_message}")
 
 # exercise 3.5 changing Guest list
-print("Exercise 3.5")
+print("\n\n\nExercise 3.5")
 
 import datetime
 invite_names = ["Donald Trump", "Vladimir Putin", "Xi Jinping", "Benjamin Netanyahu", "Abiy Ahmed"]
@@ -61,7 +61,7 @@ print(f"{date_str.rjust(50)} \nHello Mr {invite_names[1]} {invitation_message}")
 print(f"{date_str.rjust(50)} \nHello Mr {invite_names[2]} {invitation_message}")
 
 # exercise 3.6 More Guests
-print("Exercise 3.6")
+print("\n\n\nExercise 3.6")
 
 """You just found a bigger dinner tabalem so now more space is available. Think of three more guests to invite to dinner.
 - Start with your program from Exercise 3.4 or Exercise 3.5. Add a print to the end of your program informing people found
@@ -80,8 +80,8 @@ for i in range(len(invite_names)):
     print(f"{date_str.rjust(50)} \nHello Mr {invite_names[i]} {invitation_message}")
 
 # exercise 3.7 Shrinking Guest list
-print("Exercise 3.7")
-''' You just found out that your new dinner won't arrive in time for the dinner,
+print("\n\n\nExercise 3.7")
+""" You just found out that your new dinner won't arrive in time for the dinner,
 and you have space for only two guests.
 - Start with your program from Exercise 3.6. Add a new line that prints a message
 saying that you can invite only two people for dinner.
@@ -89,10 +89,8 @@ saying that you can invite only two people for dinner.
 Each time you pop a name from your list, print a message to that person letting them know you're sorry you can't invite them to dinner.
 - Print a message to each of the people still on your list, letting them know they're still invited.
 - Use delto remove the last two names from your list, so you have an empty list. 
-Print your list to make sure you actually have an empty list at the end of your program.'''
-
-# exercise 3.6 plus the following
-print("Exercise 3.6")
+Print your list to make sure you actually have an empty list at the end of your program.
+"""
 
 print("you can invite only two people for dinner.") # message written after checked there only two table are arrived for dinner.
 
@@ -103,3 +101,48 @@ while (len(invite_names)) > 2:
 while (len(invite_names) - 1) >= 0: # remove a person after writing remember card.
       print(f"Hello Mr {invite_names.pop()}, Do forget to came, You still invited to the dinner.")
 print(invite_names) #empty name list.
+
+
+# exercise 3.8 Seeing the World
+print("\n\n\nExercise 3.8")
+"""Think of at least five places in the world you'd like to visit.
+- Store the locations in a list. Make sure the list is not in alphabetical order.
+- Print your list in its orginal order. Don't worry about neatly, just print it as a row Python list.
+- Use sorted() to print your list in alphabetical order without modifying the actual list.
+- Show that your list is still in its original order by printing it.
+- Use reverse() to change the order of your list again. print the list to show that its order has changed.
+- Use reverse() to change the order of your list again. Print the list to show it's back to to its original order.
+- Use sort() to change your list so it's stored in alphabetical order. Print the list to showthat ists order has been changed.
+- Use sort() to change your list so it's stored in reverse alphabetical order. Print the list to show that its order has changed."""
+
+place_to_visit = ["Great wall of Chine, China", "Giza pyramid, Egypt", "Eiffel Tower, France", "The Ancient city of Axum, Ethiopia", "Burj Khalifa, Dubia", "The Colosseum, Italy"] #list of best place to visit.
+for place in range(len(place_to_visit)):
+      print(place_to_visit[place]) #print the list in there orginal order.
+print("\n")
+for place in range(len(place_to_visit)):
+      print(sorted(place_to_visit[place])) #print the list in there in the ascending alphabet order.
+print("\n")
+for place in range(len(place_to_visit)):
+      print(place_to_visit[place]) #print the list in there orginal order again.
+print("\n")
+for place in range(len(place_to_visit)):
+      print(sorted(place_to_visit[place], reverse = True)) #print the list in the in descending of alphabet order.
+print("\n")
+for place in range(len(place_to_visit)):
+      print(place_to_visit[place]) #print the list still its orginal order.
+print("\n")
+for place in range(len(place_to_visit)):
+      print(reversed(place_to_visit[place])) #print the list in the reversed order.
+print("\n")
+for place in range(len(place_to_visit)):
+      print(reversed(place_to_visit[place])) #print the list in the double reversed which is orginal order.
+print("\n")
+place_to_visit.sort()
+for place in range(len(place_to_visit)):
+      print(place_to_visit[place]) #print the list that permantly sorted in ascending alphabetic order.
+print("\n")
+place_to_visit.sort(reverse=True)
+for place in range(len(place_to_visit)):
+      print(place_to_visit[place]) #print the list that permantly sorted in descending alphabetic order.
+      
+      
